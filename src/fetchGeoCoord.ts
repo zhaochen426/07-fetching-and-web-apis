7bd48ef4016c5fdb794f6eb9ecfcc0cbfd4dd557
zhaochen426 <124215308+zhaochen426@users.noreply.github.com>
2023-11-16 13:45:57 -0500

    jack

--- src/fetchGeoCoord.ts
+++ src/fetchGeoCoord.ts
@@ -5,7 +5,36 @@ export interface GeoCoord {
   lon: number;
 }
 
+interface fetchresult {
+  lat: string;
+  lon: string;
+}
 export function fetchGeoCoord(query: string): Promise<GeoCoord> {
   // TODO
-  return new Promise(res => res({ lat: NaN, lon: NaN }));
+  const base = 'https://220.maxkuechen.com/geoCoord/search'
+  const url = new URL(base)
+  url.searchParams.append('q', query)
+  
+  return fetch(url.toString())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error! Status:${response.status}`)
+      }  //response.status < 200 && response.status > 299 means valid, otherwise is not  ok (200 -299)
+      return response.json()
+    })
+    .then((data:fetchresult[]) => {
+      if (!Array.isArray(data) || data.length === 0) {
+        throw new Error('No results found for query.')
+      }
+      const result:fetchresult = data[0]
+      return {
+        lat: Number.parseFloat(result.lat),
+        lon: Number.parseFloat(result.lon)
+      }
+    })
+    .catch(error => {
+      console.error("Error fetching the url", error)
+      throw error
+    })
 }
+//return new Promise(res => res({ lat: NaN, lon: NaN }));
